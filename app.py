a2c8cf112db71ae96615c1f2dfd8583a6d158c73
Shwetabh <webwritat@gmail.com>
2025-06-01 13:59:24 +0530

    change to server database

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('APP_SECRET')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///writart.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///writart.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
@@ -42,5 +42,5 @@ def load_user(member_id):
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
