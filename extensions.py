290af0f4e9c138c9e0b8c4e0f5ff9a247c843e9d
Shwetabh Suman <shwetabhartist@gmail.com>
2025-08-04 20:47:06 +0530

    initial commit dark-age

--- extensions.py
+++ extensions.py
@@ -1,13 +1,12 @@
 from functools import wraps
 from flask import abort
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user
 from flask_mail import Mail, Message
 from datetime import datetime
 
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+# db = SQLAlchemy()
 mail = Mail()
 
 
@@ -19,11 +18,11 @@ image_dict = {
 current_year = datetime.now().year
 
 
-def admin_only(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if current_user.role.name != 'admin':
-            return abort(403)
-        return f(*args, **kwargs)
-
-    return decorated_function
+# def admin_only(f):
+#     @wraps(f)
+#     def decorated_function(*args, **kwargs):
+#         if current_user.role.name != 'admin':
+#             return abort(403)
+#         return f(*args, **kwargs)
+#
+#     return decorated_function
--- routes/main.py
+++ routes/main.py
@@ -1,35 +1,11 @@
 from flask import Blueprint, render_template, request, flash, session, url_for
-from extensions import login_manager, db, current_year
-from flask_login import current_user
+from extensions import current_year
 
-from models.member import Member
 
 main = Blueprint('main', __name__, static_folder='static', template_folder='templates')
 
 
 @main.route('/', methods=['GET', 'POST'])
 def home():
+    return render_template('index.html', current_year=current_year)
 
-    return render_template('index.html', logged_in=current_user.is_authenticated,
-                           current_year=current_year)
-
-
-@main.route('/team', methods=['GET', 'POST'])
-def team():
-    return render_template('team.html', logged_in=current_user.is_authenticated,
-                           current_year=current_year)
-
-
-@main.route('/ad_samples', methods=['GET', 'POST'])
-def ad_samples():
-    return render_template('ad_samples.html', logged_in=current_user.is_authenticated,
-                           current_year=current_year)
-
-@main.route('/privacy_policy')
-def privacy_policy():
-    return render_template('privacy_policy.html', current_year=current_year)
-
-
-@main.route('/terms_and_conditions')
-def terms_and_conditions():
-    return render_template('terms_and_conditions.html', current_year=current_year)
